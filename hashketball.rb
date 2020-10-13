37d3aabe4c829917e62c5fe2d0e338679cdc9f4c
Ronalyssa Loiseau <rloiseau99@yahoo.com>
2020-10-13 21:45:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,12 +3,14 @@ def game_hash
   {}
   {
     :home => {
-      :team_name => {},
-      :colors => {},
+      :team_name => "",
+      :colors => [""],
+      :players => [{}]
   },
     :away => {
-      :team_name => {},
-      :colors => {}
+      :team_name => "",
+      :colors => [""],
+      :players => [{}]
     }
   }
 end
