dfdcf3f76c6207b5ab7d194a5575eedd28d3dbc7
Ronalyssa Loiseau <rloiseau99@yahoo.com>
2020-10-13 22:07:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -19,7 +19,7 @@ def game_hash
         {
           :player_name => "Reggie Evans",
           :number => 30,
-          :shoe => "",
+          :shoe => 14,
           :points => "",
           :rebounds => "",
           :assists => "",
